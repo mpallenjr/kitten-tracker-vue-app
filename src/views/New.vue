d46a61b1e0ae0c7760d30725eac7587fd2bbdd1c
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-17 20:23:23 -0600

    stopping point

--- src/views/New.vue
+++ src/views/New.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>Name:  <input type="text" v-model="newKitten.name"></p>
+    <p>Age:  <input type="text" v-model="newKitten.age"></p>
+    <p>Breed:  <input type="text" v-model="newKitten.breed"></p>
+    <p>Notes:  <input type="text" v-model="newKitten.notes"></p>
+    <p>Image_URL:  <input type="text" v-model="newKitten.image_url"></p>
+    <button v-on:click="newKitten">Add Kitten</button>
+
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from 'axios';
   export default {
     data: function () {
       return {
@@ -14,6 +22,22 @@
       };
     },
     created: function () {},
-    methods: {},
+    methods: {
+
+      newKitten: function() {
+        console.log('hello')
+        axios.post("http://localhost:3000/kittens",
+        {
+          name: this.newKitten.name,
+          age: this.newKitten.age,
+          breed: this.newKitten.breed,
+          image_url: this.newKitten.image_url,
+          notes: this.newKitten.notes
+
+        }).then(response => {
+          console.log(response.data)
+        })
+      }
+    },
   };
 </script>
